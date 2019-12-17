848ee210cba631cabc7fb61f6ac2dab2c3e9e189
Travis <ymon0003@vlan-2708-10-17-190-207.guest.wireless.sydney.edu.au>
2019-12-17 17:57:59 +1100

    update gitignore

--- scripts/RF_GridSearch.py
+++
@@ -1,8 +0,0 @@
-import gTranRec as gtr
-
-max_features = [2,3,4,5,6]
-min_samples_leaf = [1,2,3]
-
-param_grid = {'max_features':max_features,
-             'min_samples_leaf':min_samples_leaf}
-gtr.RF_GridSearch(param_grid, X_train, y_train)
