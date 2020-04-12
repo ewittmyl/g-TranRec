732463445ebd51bc00e2401d48e9a4ef77dfaf1e
Travis <ymon0003@Traviss-MacBook-Pro.local>
2020-04-12 22:28:50 +1000

    remove import freature extract

--- gTranRec/__init__.py
+++ gTranRec/__init__.py
@@ -2,7 +2,7 @@ from .features import *
 from .image_process import *
 from .download import *
 from .model import *
-from .data_set import *
+#from .data_set import *
 from .pipeline import *
 from .gaussian import *
 from .postprocess import *
