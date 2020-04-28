0c2ee8360c65114a969f1cfe733b4bc5eed26280
Travis <ymon0003@Traviss-MacBook-Pro.local>
2020-04-28 11:15:57 +1000

    debugging

--- gTranRec/features.py
+++ gTranRec/features.py
@@ -248,8 +248,9 @@ class CalcALL():
         amp_dict = gauss_amp.dict()
         r_dict = gauss_r.dict()
         jobs = []
+        print(cpu_count(), len(nstamps_chunks))
         for i in range(cpu_count()):
-            print(i, nstamps_chunks[i].shape)
+            # print(i, nstamps_chunks[i].shape)
             p = Process(target=chunk_fit, args=(i,nstamps_chunks[i],amp_dict,r_dict))
             jobs.append(p)
             p.start()
