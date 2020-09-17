f45a523c21908a36b23661f0bc91c4ebe88b3647
ewittmyl <yik.mong@monash.edu>
2020-09-17 19:38:56 +1000

    comment on the online mp check

--- gTranRec/catsHTM_check.py
+++ gTranRec/catsHTM_check.py
@@ -233,16 +233,16 @@ def xmatch_check(photometry_df, obsdate, srad=10, thresh=0.85, conn="gotocompute
 			if mpc.table.shape[0] > 0:
 				mp_c = skycoord(ra=mpc.table['RA_deg']*u.degree, dec=mpc.table['Dec_deg']*u.degree)
 				photometry_df.at[r[0], 'mp_offset'] = round(np.min(c1.separation(mp_c).arcsec), 2)
-	i = 1
-	for r in photometry_df.iterrows():
-		if (r[1].gtr_wcnn > thresh) & ( (r[1].known_offset>5) | np.isnan(r[1].known_offset) ) & ( (r[1].mp_offset>5) | np.isnan(r[1].mp_offset) ):
-			c1 = skycoord(r[1]['ra']*u.degree, r[1]['dec']*u.degree, frame='icrs')
-			if i % 10:
-				mpc = pympc.Checker()
-			mpc.cone_search(r[1]['ra'], r[1]['dec'],obsdate, srad, online=True)
-			i += 1
-			if mpc.table.shape[0] > 0:
-				mp_c = skycoord(ra=mpc.table['RA_deg']*u.degree, dec=mpc.table['Dec_deg']*u.degree)
-				photometry_df.at[r[0], 'mp_offset'] = round(np.min(c1.separation(mp_c).arcsec), 2)
+	# i = 1
+	# for r in photometry_df.iterrows():
+	# 	if (r[1].gtr_wcnn > thresh) & ( (r[1].known_offset>5) | np.isnan(r[1].known_offset) ) & ( (r[1].mp_offset>5) | np.isnan(r[1].mp_offset) ):
+	# 		c1 = skycoord(r[1]['ra']*u.degree, r[1]['dec']*u.degree, frame='icrs')
+	# 		if i % 10:
+	# 			mpc = pympc.Checker()
+	# 		mpc.cone_search(r[1]['ra'], r[1]['dec'],obsdate, srad, online=True)
+	# 		i += 1
+	# 		if mpc.table.shape[0] > 0:
+	# 			mp_c = skycoord(ra=mpc.table['RA_deg']*u.degree, dec=mpc.table['Dec_deg']*u.degree)
+	# 			photometry_df.at[r[0], 'mp_offset'] = round(np.min(c1.separation(mp_c).arcsec), 2)
 
 	return photometry_df
