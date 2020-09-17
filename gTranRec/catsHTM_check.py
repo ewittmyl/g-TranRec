a902eda54b362a41e50e8cceceb17a13da23b539
ewittmyl <yik.mong@monash.edu>
2020-09-17 19:31:36 +1000

    debug (AttributeError: 'float' object has no attribute 'values')

--- gTranRec/catsHTM_check.py
+++ gTranRec/catsHTM_check.py
@@ -235,7 +235,7 @@ def xmatch_check(photometry_df, obsdate, srad=10, thresh=0.85, conn="gotocompute
 				photometry_df.at[r[0], 'mp_offset'] = round(np.min(c1.separation(mp_c).arcsec), 2)
 	i = 1
 	for r in photometry_df.iterrows():
-		if (r[1].gtr_wcnn > thresh) & ( (r[1].known_offset.values>5) | np.isnan(r[1].known_offset.values) ) & ( (r[1].mp_offset.values>5) | np.isnan(r[1].mp_offset.values) ):
+		if (r[1].gtr_wcnn > thresh) & ( (r[1].known_offset>5) | np.isnan(r[1].known_offset) ) & ( (r[1].mp_offset>5) | np.isnan(r[1].mp_offset) ):
 			c1 = skycoord(r[1]['ra']*u.degree, r[1]['dec']*u.degree, frame='icrs')
 			if i % 10:
 				mpc = pympc.Checker()
